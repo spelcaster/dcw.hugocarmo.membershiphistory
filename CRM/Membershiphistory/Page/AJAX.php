3f693af5f6bc36ee4444f7e308890f4ebead139a
Hugo do Carmo <hugrito.kun@gmail.com>
2017-08-02 00:54:24 -0300

    refactoring getMembershipHistory method from AJAX page
    - The getlist now can be filtered using the contact and/or the membership
      id, those values can be passed in the GET request to be used in the
      api call;

--- CRM/Membershiphistory/Page/AJAX.php
+++ CRM/Membershiphistory/Page/AJAX.php
@@ -4,8 +4,20 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Membershiphistory_Page_AJAX extends CRM_Core_Page {
   public static function getMembershipHistory() {
+    $params = [];
+
+    if (isset($_GET['cid'])) {
+      $params['cid'] = $_GET['cid'];
+    }
+
+    if (isset($_GET['membership_id'])) {
+      $params['membership_id'] = $_GET['membership_id'];
+    }
+
     $result = civicrm_api3(
-        'MembershipHistory', 'getlist', []
+        'MembershipHistory',
+        'getlist',
+        $params
     );
 
     CRM_Utils_JSON::output($result['values']);
